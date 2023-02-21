843701a057e209310410ade180839302d13c56d3
Massimo Albanese <33076159+madlitch@users.noreply.github.com>
2023-02-21 17:42:47 -0500

    - somewhat wrote jeopardy to spec - added gitignore

---
+++ jeopardy/jeopardy.c
@@ -0,0 +1,98 @@
+/*
+ * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2015
+ * Massimo Albanese 100616057
+ * Lyba Mughees 100750490
+ * Daanyaal Tahir 100746644
+ * Malaika Sharif 100651617
+ * All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "questions.h"
+#include "players.h"
+#include "jeopardy.h"
+
+// Put macros or constants here using #define
+#define BUFFER_LEN 256
+#define NUM_PLAYERS 4
+
+// Put global environment variables here
+
+// Processes the answer from the user containing what is or who is and tokenizes it to retrieve the answer.
+void tokenize(char *input, char **tokens);
+
+int main(int argc, char *argv[]) {
+    // An array of 4 players, may need to be a pointer if you want it set dynamically
+    player players[NUM_PLAYERS];
+
+    // Input buffer and and commands
+    char buffer[BUFFER_LEN] = {0};
+
+    // Display the game introduction and initialize the questions
+    initialize_game();
+
+    // Prompt for players names
+    printf("Enter your names:\n");
+
+    // initialize each of the players in the array
+    for (int i = 0; i < NUM_PLAYERS; i++) {
+        printf("Player %d: ", i + 1);
+        scanf("%s", players[i].name);
+        update_score(players, NUM_PLAYERS, players[i].name, 0);
+    }
+
+    // Perform an infinite loop getting command input from users until game ends
+    while (fgets(buffer, BUFFER_LEN, stdin) != NULL) {
+        char category[MAX_LEN];
+        char answer[MAX_LEN];
+        int value = 0;
+        int questions_answered = 0;
+
+        while (questions_answered < NUM_QUESTIONS) {
+            for (int i = 0; i < NUM_PLAYERS; i++) {
+                // Call functions from the questions and players source files
+                display_categories();
+                start: printf("%s, enter a category, then the value\n", players[i].name);
+                printf("Category (lowercase): ");
+                scanf("%s", &category);
+                printf("Value: ");
+                scanf("%d", &value);
+
+                if (invalid_question(category, value)) {
+                    printf("Already answered/Doesn't exist!\n");
+                    goto start;
+                }
+
+                display_question(category, value);
+
+                printf("Answer (in all lowercase): ");
+                scanf("%s", answer);
+
+                if (valid_answer(category, value, answer)) {
+                    printf("That's right! You get %d points!\n", value);
+                    update_score(players, NUM_PLAYERS, players[i].name, players[i].score + value);
+                } else {
+                    printf("Sorry, the answer is '%s'!\n", question_answer(category, value));
+                }
+
+                answer_question(category, value);
+                questions_answered++;
+                show_results(players, NUM_PLAYERS);
+            }
+        }
+        break;
+    }
+    return EXIT_SUCCESS;
+}
+
+// Displays the game results for each player, their name and final score, ranked from first to last place
+void show_results(player *players, int num_players) {
+    for (int i = 0; i < num_players; i++) {
+        printf("Player: %s, Score: %d\n", players[i].name, players[i].score);
+    }
+}
---
+++ jeopardy/questions.c
@@ -0,0 +1,151 @@
+/*
+ * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2015, <GROUP MEMBERS>
+ * All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "questions.h"
+
+// Initializes the array of questions for the game
+void initialize_game(void) {
+    // initialize each question struct and assign it to the questions array
+//    q1
+    strcpy(questions[0].category, "nature");
+    strcpy(questions[0].question, "How many oceans are there?");
+    strcpy(questions[0].answer, "5");
+    questions[0].value = 100;
+    questions[0].answered = false;
+
+    strcpy(questions[1].category, "nature");
+    strcpy(questions[1].question, "How many oceans are there?");
+    strcpy(questions[1].answer, "5");
+    questions[1].value = 200;
+    questions[1].answered = false;
+
+    strcpy(questions[2].category, "nature");
+    strcpy(questions[2].question, "How many oceans are there?");
+    strcpy(questions[2].answer, "5");
+    questions[2].value = 300;
+    questions[2].answered = false;
+
+    strcpy(questions[3].category, "nature");
+    strcpy(questions[3].question, "How many oceans are there?");
+    strcpy(questions[3].answer, "5");
+    questions[3].value = 400;
+    questions[3].answered = false;
+
+    strcpy(questions[4].category, "sports");
+    strcpy(questions[4].question, "How many oceans are there?");
+    strcpy(questions[4].answer, "5");
+    questions[4].value = 100;
+    questions[4].answered = false;
+
+    strcpy(questions[5].category, "sports");
+    strcpy(questions[5].question, "How many oceans are there?");
+    strcpy(questions[5].answer, "5");
+    questions[5].value = 200;
+    questions[5].answered = false;
+
+    strcpy(questions[6].category, "sports");
+    strcpy(questions[6].question, "How many oceans are there?");
+    strcpy(questions[6].answer, "5");
+    questions[6].value = 300;
+    questions[6].answered = false;
+
+    strcpy(questions[7].category, "sports");
+    strcpy(questions[7].question, "How many oceans are there?");
+    strcpy(questions[7].answer, "5");
+    questions[7].value = 400;
+    questions[7].answered = false;
+
+    strcpy(questions[8].category, "food and drink");
+    strcpy(questions[8].question, "How many oceans are there?");
+    strcpy(questions[8].answer, "5");
+    questions[8].value = 100;
+    questions[8].answered = false;
+
+    strcpy(questions[9].category, "food and drink");
+    strcpy(questions[9].question, "How many oceans are there?");
+    strcpy(questions[9].answer, "5");
+    questions[9].value = 200;
+    questions[9].answered = false;
+
+    strcpy(questions[10].category, "food and drink");
+    strcpy(questions[10].question, "How many oceans are there?");
+    strcpy(questions[10].answer, "5");
+    questions[10].value = 300;
+    questions[10].answered = false;
+
+    strcpy(questions[11].category, "food and drink");
+    strcpy(questions[11].question, "How many oceans are there?");
+    strcpy(questions[11].answer, "5");
+    questions[11].value = 400;
+    questions[11].answered = false;
+}
+
+// Displays each of the remaining categories and question dollar values that have not been answered
+void display_categories(void) {
+    // print categories and dollar values for each unanswered question in questions array
+    char question_values[12][20];
+
+    for (int i = 0; i < 12; i++) {
+        if (!questions[i].answered) {
+            snprintf(question_values[i], 20, "%d", questions[i].value);
+        } else {
+            snprintf(question_values[i], 20, "%s", "---");
+        }
+    }
+
+    printf("---------------------------JEOPARDY---------------------------\n");
+    printf("  %s    |  %s  |  \t%s  \n", categories[0], categories[1], categories[2]);
+    printf("--------------------------------------------------------------\n");
+    printf("      %s  |  \t%s  |  \t%s  \n", question_values[0], question_values[4], question_values[8]);
+    printf("--------------------------------------------------------------\n");
+    printf("      %s  |  \t%s  |  \t%s  \n", question_values[1], question_values[5], question_values[9]);
+    printf("--------------------------------------------------------------\n");
+    printf("      %s  |  \t%s  |  \t%s  \n", question_values[2], question_values[6], question_values[10]);
+    printf("--------------------------------------------------------------\n");
+    printf("      %s  |  \t%s  |  \t%s  \n", question_values[3], question_values[7], question_values[11]);
+    printf("--------------------------------------------------------------\n");
+}
+
+// Displays the question for the category and dollar value
+void display_question(char *category, int value) {
+    question q = questions[question_index_lookup(category, value)];
+    printf("Question: %s, Value: %d\n", q.question, q.value);
+}
+
+// Returns true if the answer is correct for the question for that category and dollar value
+bool valid_answer(char *category, int value, char *answer) {
+    int i = question_index_lookup(category, value);
+    return (strcmp(questions[i].answer, answer) == 0);
+}
+
+// Returns true if the question has already been answered or question doesn't exist
+bool invalid_question(char *category, int value) {
+    int i = question_index_lookup(category, value);
+    if (i < 0)
+        return true;
+    return questions[i].answered;
+}
+
+void answer_question(char *category, int value) {
+    questions[question_index_lookup(category, value)].answered = true;
+}
+
+char* question_answer(char *category, int value) {
+    return questions[question_index_lookup(category, value)].answer;
+}
+
+int question_index_lookup(char category[], int value) {
+    for (int i = 0; i < NUM_QUESTIONS; i++) {
+        if (strcmp(questions[i].category, category) == 0 && questions[i].value == value) {
+            return i;
+        }
+    }
+    return -1;
+}
