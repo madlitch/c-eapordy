9bd456f7ef5a36333f8672be60166209153e6038
lybamughees <66569488+lybamughees@users.noreply.github.com>
2023-02-22 16:05:16 -0500

    Update questions.c

--- jeopardy/questions.c
+++ jeopardy/questions.c
@@ -21,68 +21,68 @@ void initialize_game(void) {
     questions[0].answered = false;
 
     strcpy(questions[1].category, "nature");
-    strcpy(questions[1].question, "How many oceans are there?");
-    strcpy(questions[1].answer, "5");
+    strcpy(questions[1].question, "What percent of Earth is water?");
+    strcpy(questions[1].answer, "72");
     questions[1].value = 200;
     questions[1].answered = false;
 
     strcpy(questions[2].category, "nature");
-    strcpy(questions[2].question, "How many oceans are there?");
-    strcpy(questions[2].answer, "5");
+    strcpy(questions[2].question, "What is the fastest animal?");
+    strcpy(questions[2].answer, "cheetah");
     questions[2].value = 300;
     questions[2].answered = false;
 
     strcpy(questions[3].category, "nature");
-    strcpy(questions[3].question, "How many oceans are there?");
-    strcpy(questions[3].answer, "5");
+    strcpy(questions[3].question, "What is another name for the fall?");
+    strcpy(questions[3].answer, "autumn");
     questions[3].value = 400;
     questions[3].answered = false;
 
     strcpy(questions[4].category, "sports");
-    strcpy(questions[4].question, "How many oceans are there?");
-    strcpy(questions[4].answer, "5");
+    strcpy(questions[4].question, "What sport is played at the NBA championships?");
+    strcpy(questions[4].answer, "basketball");
     questions[4].value = 100;
     questions[4].answered = false;
 
     strcpy(questions[5].category, "sports");
-    strcpy(questions[5].question, "How many oceans are there?");
-    strcpy(questions[5].answer, "5");
+    strcpy(questions[5].question, "What league is the super-bowl?");
+    strcpy(questions[5].answer, "nfl");
     questions[5].value = 200;
     questions[5].answered = false;
 
     strcpy(questions[6].category, "sports");
-    strcpy(questions[6].question, "How many oceans are there?");
-    strcpy(questions[6].answer, "5");
+    strcpy(questions[6].question, "What team does Lebron James play for?");
+    strcpy(questions[6].answer, "lakers");
     questions[6].value = 300;
     questions[6].answered = false;
 
     strcpy(questions[7].category, "sports");
-    strcpy(questions[7].question, "How many oceans are there?");
-    strcpy(questions[7].answer, "5");
+    strcpy(questions[7].question, "What is Toronto's basketball team called?");
+    strcpy(questions[7].answer, "raptors");
     questions[7].value = 400;
     questions[7].answered = false;
 
     strcpy(questions[8].category, "food and drink");
-    strcpy(questions[8].question, "How many oceans are there?");
-    strcpy(questions[8].answer, "5");
+    strcpy(questions[8].question, "Beans & oatmeal (not necessarily together) are good sources of this substance that helps food pass through the body?");
+    strcpy(questions[8].answer, "fibre");
     questions[8].value = 100;
     questions[8].answered = false;
 
     strcpy(questions[9].category, "food and drink");
-    strcpy(questions[9].question, "How many oceans are there?");
-    strcpy(questions[9].answer, "5");
+    strcpy(questions[9].question, "It is not for children, it can be red or white ");
+    strcpy(questions[9].answer, "wine");
     questions[9].value = 200;
     questions[9].answered = false;
 
     strcpy(questions[10].category, "food and drink");
-    strcpy(questions[10].question, "How many oceans are there?");
-    strcpy(questions[10].answer, "5");
+    strcpy(questions[10].question, "It is meat from a cow?");
+    strcpy(questions[10].answer, "steak");
     questions[10].value = 300;
     questions[10].answered = false;
 
     strcpy(questions[11].category, "food and drink");
-    strcpy(questions[11].question, "How many oceans are there?");
-    strcpy(questions[11].answer, "5");
+    strcpy(questions[11].question, "You can look through it, we should drink it every day, it is healthy?");
+    strcpy(questions[11].answer, "water");
     questions[11].value = 400;
     questions[11].answered = false;
 }
